c9b4c129707ce2fd989b02f681fc3f82e1d37471
tichmajena <tichmajena@gmail.com>
2021-08-13 09:21:44 +0200

    routes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,6 +7,7 @@
   import img6 from "$lib/Vegetable-fruit-sweet-pea-seeds-Snow-peas-seed-Bonsai-plants-Seeds-for-home-garden-150-seeds.jpg";
   import picture from "/media/image1.jpeg";
   import Animate from "$lib/Animate.svelte";
+  import ProductInsights from "$lib/components/ProductInsights.svelte";
 </script>
 
 <Animate>
@@ -46,6 +47,51 @@
               >
             </div>
           </div>
+          <section>
+            <div>
+              <!-- <div class="flex flex-col text-center w-full mb-20">
+      <h1 class="text-2xl font-medium title-font mb-4 text-gray-900">
+        OUR TEAM
+      </h1>
+      <p class="lg:w-2/3 mx-auto leading-relaxed text-base">
+        Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
+        gentrify, subway tile poke farm-to-table. Franzen you probably haven't
+        heard of them.
+      </p>
+    </div> -->
+              <div class="grid grid-cols-4 gap-4">
+                <ProductInsights img_src="https://dummyimage.com/200x200">
+                  <span slot="statistic">5000kg</span>
+                  <span slot="produce">Fresh Tomatoes</span>
+                </ProductInsights>
+
+                <ProductInsights img_src="https://dummyimage.com/200x200">
+                  <span slot="statistic">3000kg</span>
+                  <span slot="produce">Dried Tomatoes</span>
+                </ProductInsights>
+
+                <ProductInsights img_src="https://dummyimage.com/200x200">
+                  <span slot="statistic">5000kg</span>
+                  <span slot="produce">Fresh Tomatoes</span>
+                </ProductInsights>
+
+                <ProductInsights img_src="https://dummyimage.com/200x200">
+                  <span slot="statistic">3000kg</span>
+                  <span slot="produce">Dried Tomatoes</span>
+                </ProductInsights>
+
+                <ProductInsights img_src="https://dummyimage.com/200x200">
+                  <span slot="statistic">5000kg</span>
+                  <span slot="produce">Fresh Tomatoes</span>
+                </ProductInsights>
+
+                <ProductInsights img_src="https://dummyimage.com/200x200">
+                  <span slot="statistic">3000kg</span>
+                  <span slot="produce">Dried Tomatoes</span>
+                </ProductInsights>
+              </div>
+            </div>
+          </section>
           <div class="flex flex-wrap -m-4">
             <div class="lg:w-1/3 sm:w-1/2 p-4">
               <div class="flex relative">
